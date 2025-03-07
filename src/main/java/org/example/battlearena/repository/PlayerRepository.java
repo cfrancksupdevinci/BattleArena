0bf170f3809890b845affc7b1d859875fa81d7c1
dragan.ribes <dragan.ribes@gmail.com>
2025-03-07 12:01:06 +0100

    test repository

--- src/main/java/org/example/battlearena/repository/PlayerRepository.java
+++ src/main/java/org/example/battlearena/repository/PlayerRepository.java
@@ -1,10 +1,11 @@
 package org.example.battlearena.repository;
 
 import org.example.battlearena.model.Player;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public class PlayerRepository{
+public class PlayerRepository implements CrudRepository<Player, Long> {
 
     public List<Player> getAllPlayer() {
         return null;
